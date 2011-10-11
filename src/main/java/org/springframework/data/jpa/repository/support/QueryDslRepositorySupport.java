036b6afa737bd335b09746730b24d33607cde1b1
Oliver Gierke <info@olivergierke.de>
2011-10-11 12:57:05 +0200

    DATAJPA-113 - Improved DI for QueryDslRepositorySupport.
    Removed @Required annotation from the setter for EntityManager property. The annotation triggered a dependency check that was not aware of the EntityManager being injected by a PersistenceAnnotationBeanPostProcessor. As we already have a validation callback using @PostConstruct we can simply rely on the not-null check for the EntityManager being implemented there.

--- src/main/java/org/springframework/data/jpa/repository/support/QueryDslRepositorySupport.java
+++ src/main/java/org/springframework/data/jpa/repository/support/QueryDslRepositorySupport.java
@@ -1,10 +1,24 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.data.jpa.repository.support;
 
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.Assert;
 
@@ -35,7 +49,6 @@ public abstract class QueryDslRepositorySupport {
 	 * 
 	 * @param entityManager must not be {@literal null}
 	 */
-	@Required
 	public void setEntityManager(EntityManager entityManager) {
 
 		Assert.notNull(entityManager);
--- src/test/java/org/springframework/data/jpa/repository/support/QueryDslRepositorySupportTests.java
+++ src/test/java/org/springframework/data/jpa/repository/support/QueryDslRepositorySupportTests.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.data.jpa.repository.support;
 
 import static org.hamcrest.Matchers.*;
@@ -99,7 +114,7 @@ public class QueryDslRepositorySupportTests {
 		repositoryImpl.validate();
 	}
 
-	private static interface UserRepository {
+	interface UserRepository {
 
 		List<User> findUsersByLastname(String firstname);
 
@@ -108,7 +123,7 @@ public class QueryDslRepositorySupportTests {
 		long deleteAllWithLastname(String lastname);
 	}
 
-	private static class UserRepositoryImpl extends QueryDslRepositorySupport implements UserRepository {
+	static class UserRepositoryImpl extends QueryDslRepositorySupport implements UserRepository {
 
 		private static final QUser user = QUser.user;
 
