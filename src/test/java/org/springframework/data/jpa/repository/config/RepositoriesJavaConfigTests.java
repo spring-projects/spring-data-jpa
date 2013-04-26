617a254112368295a1934185cb8cc9d3ba63bfbf
Oliver Gierke <ogierke@vmware.com>
2013-04-26 10:27:33 +0200

    DATAJPA-323 - Added integration tests for JavaConfig and Repositories.

---
+++ src/test/java/org/springframework/data/jpa/repository/config/RepositoriesJavaConfigTests.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.jpa.repository.config;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.data.jpa.domain.sample.User;
+import org.springframework.data.jpa.repository.sample.UserRepository;
+import org.springframework.data.repository.support.Repositories;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Integration test for the combination of JavaConfig and an {@link Repositories} wrapper.
+ * 
+ * @author Oliver Gierke
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class RepositoriesJavaConfigTests {
+
+	@Configuration
+	@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+	@ImportResource("classpath:infrastructure.xml")
+	static class Config {
+
+		@Autowired
+		ApplicationContext context;
+
+		@Bean
+		public Repositories repositories() {
+			return new Repositories(context);
+		}
+	}
+
+	@Autowired
+	Repositories repositories;
+
+	/**
+	 * @see DATAJPA-323
+	 */
+	@Test
+	public void foo() {
+		assertThat(repositories.hasRepositoryFor(User.class), is(true));
+	}
+}
