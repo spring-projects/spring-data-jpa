9499a19f09149ed24acfa24e46b6bfb8323436ca
Thomas Darimont <tdarimont@gopivotal.com>
2013-07-23 16:51:43 +0200

    DATAJPA-370 - Added missing package-info.java files.

---
+++ src/main/java/org/springframework/data/jpa/mapping/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * JPA specific support classes for the Spring Data mapping subsystem. 
+ */
+package org.springframework.data.jpa.mapping;
+
---
+++ src/main/java/org/springframework/data/jpa/repository/cdi/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * CDI support for Spring Data JPA Repositories. 
+ */
+package org.springframework.data.jpa.repository.cdi;
+
