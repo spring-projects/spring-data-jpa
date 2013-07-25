5e5f494ac59c41dfdc77518cf7039eb10520f04d
Thomas Darimont <tdarimont@gopivotal.com>
2013-07-25 17:53:04 +0200

    DATAJPA-377 - Remove "order by"-part from generated count query.
    Adjusted createCountQueryFor(…) in QueryUtils to leave out the order by part in the generated query. This avoids problems with databases that require columns specified in the order by clause to be in the select / group by list for count queries (e.g. H2). In addition to that this should give us a little performance boost if the database did not already optimize the query execution.
    
    Original pull request: #29.

--- src/test/java/org/springframework/data/jpa/repository/query/QueryUtilsUnitTests.java
+++ src/test/java/org/springframework/data/jpa/repository/query/QueryUtilsUnitTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008-2012 the original author or authors.
+ * Copyright 2008-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,6 +29,7 @@ import org.springframework.data.domain.Sort;
  * Unit test for {@link QueryUtils}.
  * 
  * @author Oliver Gierke
+ * @author Thomas Darimont
  */
 public class QueryUtilsUnitTests {
 
@@ -210,7 +211,7 @@ public class QueryUtilsUnitTests {
 	public void usesReturnedVariableInCOuntProjectionIfSet() {
 
 		assertCountQuery("select distinct m.genre from Media m where m.user = ?1 order by m.genre asc",
-				"select count(distinct m.genre) from Media m where m.user = ?1 order by m.genre asc");
+				"select count(distinct m.genre) from Media m where m.user = ?1");
 	}
 
 	/**
@@ -231,6 +232,16 @@ public class QueryUtilsUnitTests {
 
 		Sort sort = new Sort("sum(foo)");
 		assertThat(applySorting("select p from Person p", sort, "p"), endsWith("order by sum(foo) asc"));
+	}
+
+	/**
+	 * @see DATAJPA-377
+	 */
+	@Test
+	public void removesOrderByInGeneratedCountQueryFromOriginalQueryIfPresent() {
+
+		assertCountQuery("select distinct m.genre from Media m where m.user = ?1 OrDer  By   m.genre ASC",
+				"select count(distinct m.genre) from Media m where m.user = ?1");
 	}
 
 	private void assertCountQuery(String originalQuery, String countQuery) {
