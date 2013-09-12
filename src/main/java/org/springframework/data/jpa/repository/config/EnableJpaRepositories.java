3249f37b503f0d2752e829afcd2b4f6de994321d
Pei-Tang Huang <tangtheone@gmail.com>
2013-09-12 22:04:12 +0800

    DATAJPA-715 - Fixed typo in JavaDoc of @EnableJpaRepositories.

--- src/main/java/org/springframework/data/jpa/repository/config/EnableJpaRepositories.java
+++ src/main/java/org/springframework/data/jpa/repository/config/EnableJpaRepositories.java
@@ -87,7 +87,7 @@ public @interface EnableJpaRepositories {
 
 	/**
 	 * Configures the location of where to find the Spring Data named queries properties file. Will default to
-	 * {@code META-INFO/jpa-named-queries.properties}.
+	 * {@code META-INF/jpa-named-queries.properties}.
 	 * 
 	 * @return
 	 */
