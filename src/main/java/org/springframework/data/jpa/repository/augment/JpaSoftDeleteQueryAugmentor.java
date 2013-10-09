10eabc86b030fc4083e2520d97e94eeee102936e
Oliver Gierke <ogierke@gopivotal.com>
2013-10-09 16:03:12 +0200

    DATAJPA-307 - Workarounds for EclipseLink not detecting field value changes triggerd by reflection.
    We now explicitly invoke an EclipseLink specific change-tracking method in case field access is used.

--- src/main/java/org/springframework/data/jpa/repository/augment/JpaSoftDeleteQueryAugmentor.java
+++ src/main/java/org/springframework/data/jpa/repository/augment/JpaSoftDeleteQueryAugmentor.java
@@ -15,16 +15,21 @@
  */
 package org.springframework.data.jpa.repository.augment;
 
+import java.lang.reflect.Method;
+
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 
+import org.springframework.beans.BeanWrapper;
 import org.springframework.data.jpa.repository.support.JpaCriteriaQueryContext;
 import org.springframework.data.jpa.repository.support.JpaQueryContext;
 import org.springframework.data.jpa.repository.support.JpaUpdateContext;
 import org.springframework.data.repository.SoftDelete;
 import org.springframework.data.repository.augment.AbstractSoftDeleteQueryAugmentor;
 import org.springframework.data.repository.augment.QueryContext.QueryMode;
+import org.springframework.data.util.DirectFieldAccessFallbackBeanWrapper;
+import org.springframework.util.ReflectionUtils;
 
 /**
  * JPA implementation of {@link AbstractSoftDeleteQueryAugmentor} to transparently turn delete calls into entity
@@ -77,5 +82,55 @@ public class JpaSoftDeleteQueryAugmentor extends
 	@Override
 	public void updateDeletedState(Object entity, JpaUpdateContext<?> context) {
 		context.getEntityManager().merge(entity);
+	}
+
+	/* 
+	 * (non-Javadoc)
+	 * @see org.springframework.data.repository.augment.AbstractSoftDeleteQueryAugmentor#prepareBeanWrapper(org.springframework.data.repository.augment.UpdateContext)
+	 */
+	@Override
+	protected BeanWrapper createBeanWrapper(JpaUpdateContext<?> context) {
+		return new PropertyChangeEnsuringBeanWrapper(context.getEntity());
+	}
+
+	/**
+	 * Custom {@link DirectFieldAccessFallbackBeanWrapper} to hook in additional functionality when setting a property by
+	 * field access.
+	 * 
+	 * @author Oliver Gierke
+	 */
+	private static class PropertyChangeEnsuringBeanWrapper extends DirectFieldAccessFallbackBeanWrapper {
+
+		public PropertyChangeEnsuringBeanWrapper(Object entity) {
+			super(entity);
+		}
+
+		/**
+		 * We in case of setting the value using field access, we need to make sure that EclipseLink detects the change.
+		 * Hence we check for an EclipseLink specific generated method that is used to record the changes and invoke it if
+		 * available.
+		 * 
+		 * @see org.springframework.data.support.DirectFieldAccessFallbackBeanWrapper#setPropertyUsingFieldAccess(java.lang.String,
+		 *      java.lang.Object)
+		 */
+		@Override
+		public void setPropertyValue(String propertyName, Object value) {
+
+			Object oldValue = getPropertyValue(propertyName);
+			super.setPropertyValue(propertyName, value);
+			triggerPropertyChangeMethodIfAvailable(propertyName, oldValue, value);
+		}
+
+		private void triggerPropertyChangeMethodIfAvailable(String propertyName, Object oldValue, Object value) {
+
+			Method method = ReflectionUtils.findMethod(getWrappedClass(), "_persistence_propertyChange", String.class,
+					Object.class, Object.class);
+
+			if (method == null) {
+				return;
+			}
+
+			ReflectionUtils.invokeMethod(method, getWrappedInstance(), propertyName, oldValue, value);
+		}
 	}
 }
