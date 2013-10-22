2b08a0508528755d2e5b9772fb383d13d77b08b4
Thomas Darimont <tdarimont@gopivotal.com>
2013-10-22 11:57:34 +0200

    DATAJPA-416 - Add support for nested repositories.
    Support for considering nested repository interfaces can now be configured on the EnableJpaRepositories annotation via the considerNestedRepositories property. Previously nested repository definitions were ignored by the repositories infrastructure. This depends on DATACMNS-90.
    
    Original pull request: #46.

---
+++ src/test/java/org/springframework/data/jpa/repository/config/AllowNestedRepositoriesRepositoryConfigTests.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2008-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.jpa.repository.config;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.sample.ClassWithNestedRepository.NestedUserRepository;
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * Integration test for repository name-space configuration with nested repositories.
+ * 
+ * @author Thomas Darimont
+ */
+@ContextConfiguration(locations = "classpath:config/namespace-nested-repositories-application-context.xml")
+public class AllowNestedRepositoriesRepositoryConfigTests extends AbstractRepositoryConfigTests {
+
+	@Autowired NestedUserRepository fooRepository;
+
+	/**
+	 * @see DATAJPA-416
+	 */
+	@Test
+	public void shouldFindNestedRepository() {
+
+		assertThat(fooRepository, is(notNullValue()));
+	}
+}
