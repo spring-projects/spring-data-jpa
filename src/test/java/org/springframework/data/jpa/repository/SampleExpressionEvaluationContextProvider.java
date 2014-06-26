1879fea1f03ff4328fa06b2bb9742f5bbdabf38c
Thomas Darimont <tdarimont@gopivotal.com>
2014-06-26 23:12:26 +0200

    DATAJPA-564 - Expose invocation arguments as well as delegate root object properties via root expressions.
    We now use the rootObject of the delegates context as the rootObject for the expression evaluation as well as the arguments of the current method call are available via positional indices as well via the special variable called "args".
    
    Added minimalistic Security Context infrastructure that mimics that from Spring Security to ease testing.
    
    Referenced appropriate JIRA issues.

---
+++ src/test/java/org/springframework/data/jpa/repository/SampleExpressionEvaluationContextProvider.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.jpa.repository;
+
+import org.springframework.data.jpa.repository.SampleSecurity.SampleSecurityContextHolder;
+import org.springframework.data.jpa.repository.support.ExpressionEvaluationContextProvider;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+/**
+ * @author Thomas Darimont
+ */
+public class SampleExpressionEvaluationContextProvider implements ExpressionEvaluationContextProvider {
+
+	@Override
+	public EvaluationContext getEvaluationContext() {
+		return new StandardEvaluationContext(SampleSecurityContextHolder.getCurrent());
+	}
+}
