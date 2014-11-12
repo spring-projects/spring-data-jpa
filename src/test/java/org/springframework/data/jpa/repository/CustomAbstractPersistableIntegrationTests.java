4a70055a798fc6c70b056646d7453f629106573b
Thomas Darimont <tdarimont@pivotal.io>
2014-11-12 18:27:49 +0100

    DATAJPA-622 - AbstractPersistable.isNew() is now annotated with @Transient.
    We now mark AbstractPersistable.isNew() property as @Transient for JPA in order to make sure that no JPA provider complains about a missing setter in case a user derives a domain entity from AbstractPersistable. Without this it isn't possible to bootstrap a PersistenceContext.
    
    Original pull request: #115.

---
+++ src/test/java/org/springframework/data/jpa/repository/CustomAbstractPersistableIntegrationTests.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.jpa.repository;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.sample.CustomAbstractPersistable;
+import org.springframework.data.jpa.repository.sample.CustomAbstractPersistableRepository;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author Thomas Darimont
+ */
+@Transactional
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:config/namespace-autoconfig-context.xml" })
+public class CustomAbstractPersistableIntegrationTests {
+
+	@Autowired CustomAbstractPersistableRepository repository;
+
+	/**
+	 * @see DATAJPA-622
+	 */
+	@Test
+	public void shouldBeAbleToSaveAndLoadCustomPersistableWithUuidId() {
+
+		CustomAbstractPersistable entity = new CustomAbstractPersistable();
+		CustomAbstractPersistable saved = repository.save(entity);
+
+		CustomAbstractPersistable found = repository.findOne(saved.getId());
+
+		assertThat(found, is(saved));
+	}
+
+}
