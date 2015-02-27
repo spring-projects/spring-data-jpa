52a101f15557666dd34d8e98779466fca9b3d661
Oliver Gierke <ogierke@pivotal.io>
2015-02-27 19:13:32 +0100

    DATAJPA-681 - Adhoc stored procedures now automatically adapt output parameter binding based on input parameters.
    If input parameters are named we now also bind the synthetic output parameter by name for adhoc stored procedures.

--- src/test/java/org/springframework/data/jpa/repository/query/StoredProcedureAttributeSourceUnitTests.java
+++ src/test/java/org/springframework/data/jpa/repository/query/StoredProcedureAttributeSourceUnitTests.java
@@ -65,7 +65,7 @@ public class StoredProcedureAttributeSourceUnitTests {
 
 		assertThat(attr.getProcedureName(), is("plus1inout"));
 		assertThat(attr.getOutputParameterType(), is(typeCompatibleWith(Integer.class)));
-		assertThat(attr.getOutputParameterName(), is(nullValue()));
+		assertThat(attr.getOutputParameterName(), is(StoredProcedureAttributes.SYNTHETIC_OUTPUT_PARAMETER_NAME));
 	}
 
 	/**
@@ -79,7 +79,7 @@ public class StoredProcedureAttributeSourceUnitTests {
 
 		assertThat(attr.getProcedureName(), is("plus1inout"));
 		assertThat(attr.getOutputParameterType(), is(typeCompatibleWith(Integer.class)));
-		assertThat(attr.getOutputParameterName(), is(nullValue()));
+		assertThat(attr.getOutputParameterName(), is(StoredProcedureAttributes.SYNTHETIC_OUTPUT_PARAMETER_NAME));
 	}
 
 	/**
@@ -93,7 +93,7 @@ public class StoredProcedureAttributeSourceUnitTests {
 
 		assertThat(attr.getProcedureName(), is("plus1inout"));
 		assertThat(attr.getOutputParameterType(), is(typeCompatibleWith(Integer.class)));
-		assertThat(attr.getOutputParameterName(), is(nullValue()));
+		assertThat(attr.getOutputParameterName(), is(StoredProcedureAttributes.SYNTHETIC_OUTPUT_PARAMETER_NAME));
 	}
 
 	/**
@@ -107,7 +107,7 @@ public class StoredProcedureAttributeSourceUnitTests {
 
 		assertThat(attr.getProcedureName(), is("plus1inout"));
 		assertThat(attr.getOutputParameterType(), is(typeCompatibleWith(Integer.class)));
-		assertThat(attr.getOutputParameterName(), is(nullValue()));
+		assertThat(attr.getOutputParameterName(), is(StoredProcedureAttributes.SYNTHETIC_OUTPUT_PARAMETER_NAME));
 	}
 
 	/**
