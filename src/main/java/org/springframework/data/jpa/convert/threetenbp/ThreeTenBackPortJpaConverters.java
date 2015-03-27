099bba9422803b58c79368b757b6f66be271510c
Oliver Gierke <ogierke@pivotal.io>
2015-03-27 15:40:51 +0100

    DATAJPA-695 - Fixed Javadoc in JSR-310 and ThreeTen Backport converters.

--- src/main/java/org/springframework/data/jpa/convert/threetenbp/ThreeTenBackPortJpaConverters.java
+++ src/main/java/org/springframework/data/jpa/convert/threetenbp/ThreeTenBackPortJpaConverters.java
@@ -37,7 +37,7 @@ import org.threeten.bp.LocalTime;
 /**
  * JPA 2.1 converters to turn ThreeTen back port types into legacy {@link Date}s. To activate these converters make sure
  * your persistence provider detects them by including this class in the list of mapped classes. In Spring environments,
- * you can simply register the package of this class (i.e. {@code org.springframework.data.jpa.domain.support}) as
+ * you can simply register the package of this class (i.e. {@code org.springframework.data.jpa.convert.threetenbp}) as
  * package to be scanned on e.g. the {@link LocalContainerEntityManagerFactoryBean}.
  * 
  * @author Oliver Gierke
