ada8f9acb6adaf5a33a23ebd3e165257f73aa42b
Marvin Bredal Lillehaug <marlil@kantega.no>
2015-03-27 15:14:26 +0100

    Wrong package reference in Jsr310JpaConverters and ThreeTenBackPortJpaConverters

--- src/main/java/org/springframework/data/jpa/convert/threeten/Jsr310JpaConverters.java
+++ src/main/java/org/springframework/data/jpa/convert/threeten/Jsr310JpaConverters.java
@@ -37,9 +37,9 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 /**
  * JPA 2.1 converters to turn JSR-310 types into legacy {@link Date}s. To activate these converters make sure your
  * persistence provider detects them by including this class in the list of mapped classes. In Spring environments, you
- * can simply register the package of this class (i.e. {@code org.springframework.data.jpa.domain.support}) as package
+ * can simply register the package of this class (i.e. {@code org.springframework.data.jpa.convert.threeten}) as package
  * to be scanned on e.g. the {@link LocalContainerEntityManagerFactoryBean}.
- * 
+ *
  * @author Oliver Gierke
  */
 public class Jsr310JpaConverters {
