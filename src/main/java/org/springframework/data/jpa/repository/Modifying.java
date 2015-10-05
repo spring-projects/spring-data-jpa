dddadd454750740552d127612161db6867aba19a
Oliver Gierke <ogierke@pivotal.io>
2015-10-05 12:30:43 +0200

    DATAJPA-805 - Fixed Javadoc for @Modifying.clearAutomatically.

--- src/main/java/org/springframework/data/jpa/repository/Modifying.java
+++ src/main/java/org/springframework/data/jpa/repository/Modifying.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008-2011 the original author or authors.
+ * Copyright 2008-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,7 +32,7 @@ import java.lang.annotation.Target;
 public @interface Modifying {
 
 	/**
-	 * Defines whether we should clear the underlying persistence context after excuting the modifying query.
+	 * Defines whether we should clear the underlying persistence context after executing the modifying query.
 	 * 
 	 * @return
 	 */
