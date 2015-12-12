3c0dbdab0f1f88b373f82f865430220797f0d01a
Oliver Gierke <ogierke@pivotal.io>
2015-12-12 09:47:10 +0100

    DATAJPA-834 - Polishing.
    Updated copyright date range and added author.
    
    Original pull request: #159.

--- src/main/java/org/springframework/data/jpa/repository/config/BeanDefinitionNames.java
+++ src/main/java/org/springframework/data/jpa/repository/config/BeanDefinitionNames.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 the original author or authors.
+ * Copyright 2014-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@ package org.springframework.data.jpa.repository.config;
  * 
  * @author Oliver Gierke
  * @author Thomas Darimont
+ * @author Andrew Walters
  */
 interface BeanDefinitionNames {
 
