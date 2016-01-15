88d344d75c568a13d20435e5ebcd6b9d1f32bef5
Oliver Gierke <ogierke@pivotal.io>
2016-01-15 14:03:23 +0100

    DATAJPA-848 - Fixed AbstractPersistable's equals(…) for proxies.
    AbstractPersistable.equals(…) now consides the target object's user class before comparing types.

--- src/test/java/org/springframework/data/jpa/repository/sample/CustomAbstractPersistableRepository.java
+++ src/test/java/org/springframework/data/jpa/repository/sample/CustomAbstractPersistableRepository.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 the original author or authors.
+ * Copyright 2014-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,9 +18,10 @@ package org.springframework.data.jpa.repository.sample;
 import java.util.UUID;
 
 import org.springframework.data.jpa.domain.sample.CustomAbstractPersistable;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * @author Thomas Darimont
+ * @author Oliver Gierke
  */
-public interface CustomAbstractPersistableRepository extends CrudRepository<CustomAbstractPersistable, UUID> {}
+public interface CustomAbstractPersistableRepository extends JpaRepository<CustomAbstractPersistable, UUID> {}
