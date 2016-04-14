696a3b8761834918cd216a13a4b6fbaa2bb665b3
Oliver Gierke <ogierke@pivotal.io>
2016-04-14 17:07:31 +0200

    DATAJPA-891 - Polishing.
    Formatting in JpaPersistentPropertyImpl.

--- src/main/java/org/springframework/data/jpa/mapping/JpaPersistentPropertyImpl.java
+++ src/main/java/org/springframework/data/jpa/mapping/JpaPersistentPropertyImpl.java
@@ -57,8 +57,8 @@ import org.springframework.util.Assert;
  * @author Greg Turnquist
  * @since 1.3
  */
-class JpaPersistentPropertyImpl extends AnnotationBasedPersistentProperty<JpaPersistentProperty> implements
-		JpaPersistentProperty {
+class JpaPersistentPropertyImpl extends AnnotationBasedPersistentProperty<JpaPersistentProperty>
+		implements JpaPersistentProperty {
 
 	private static final Collection<Class<? extends Annotation>> ASSOCIATION_ANNOTATIONS;
 	private static final Collection<Class<? extends Annotation>> ID_ANNOTATIONS;
@@ -130,8 +130,8 @@ class JpaPersistentPropertyImpl extends AnnotationBasedPersistentProperty<JpaPer
 	 */
 	@Override
 	public Iterable<? extends TypeInformation<?>> getPersistentEntityType() {
-		return associationTargetType == null ? super.getPersistentEntityType() : Collections
-				.singleton(associationTargetType);
+		return associationTargetType == null ? super.getPersistentEntityType()
+				: Collections.singleton(associationTargetType);
 	}
 
 	/* 
@@ -241,7 +241,8 @@ class JpaPersistentPropertyImpl extends AnnotationBasedPersistentProperty<JpaPer
 	 */
 	private Boolean detectPropertyAccess() {
 
-		org.springframework.data.annotation.AccessType accessType = findAnnotation(org.springframework.data.annotation.AccessType.class);
+		org.springframework.data.annotation.AccessType accessType = findAnnotation(
+				org.springframework.data.annotation.AccessType.class);
 
 		if (accessType != null) {
 			return Type.PROPERTY.equals(accessType.value());
