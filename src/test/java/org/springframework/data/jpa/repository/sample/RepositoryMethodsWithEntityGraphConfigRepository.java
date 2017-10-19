f0bf54c01bff81613e7a42fb5eafb2d2f0f39e48
Jens Schauder <jschauder@pivotal.io>
2017-10-19 14:09:32 +0200

    DATAJPA-1207 - Apply fetch graph to queries from specifications.
    This just adds a test since the fetch graph already does get applied.
    
    Original Pull Request: #229

--- src/test/java/org/springframework/data/jpa/repository/sample/RepositoryMethodsWithEntityGraphConfigRepository.java
+++ src/test/java/org/springframework/data/jpa/repository/sample/RepositoryMethodsWithEntityGraphConfigRepository.java
@@ -20,11 +20,14 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.domain.sample.User;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.lang.Nullable;
 
 import com.querydsl.core.types.Predicate;
 
@@ -35,9 +38,10 @@ import com.querydsl.core.types.Predicate;
  * @author Thomas Darimont
  * @author Jocelyn Ntakpe
  * @author Christoph Strobl
+ * @author Jens Schauder
  */
 public interface RepositoryMethodsWithEntityGraphConfigRepository
-		extends CrudRepository<User, Integer>, QuerydslPredicateExecutor<User> {
+		extends CrudRepository<User, Integer>, QuerydslPredicateExecutor<User>, JpaSpecificationExecutor {
 
 	/**
 	 * Should find all users.
@@ -62,6 +66,11 @@ public interface RepositoryMethodsWithEntityGraphConfigRepository
 	// DATAJPA-790
 	@EntityGraph("User.detail")
 	Page<User> findAll(Predicate predicate, Pageable pageable);
+
+	// DATAJPA-1207
+	@Override
+	@EntityGraph("User.detail")
+	Page<User> findAll(@Nullable Specification spec, Pageable pageable);
 
 	// DATAJPA-1041
 	@EntityGraph(type = EntityGraphType.FETCH, value = "User.withSubGraph")
