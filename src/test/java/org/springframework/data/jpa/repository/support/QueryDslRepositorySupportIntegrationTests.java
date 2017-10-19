96ccca2987f5c62921e5229e55f09fc48f3c5e79
Oliver Gierke <ogierke@pivotal.io>
2017-10-19 15:16:54 +0200

    DATAJPA-1205 - Polishing.
    Declare BeanFactoryPostProcessor as static bean.
    
    Related ticket: DATAJPA-1175.
    Original pull request: #227.

--- src/test/java/org/springframework/data/jpa/repository/support/QueryDslRepositorySupportIntegrationTests.java
+++ src/test/java/org/springframework/data/jpa/repository/support/QueryDslRepositorySupportIntegrationTests.java
@@ -63,7 +63,7 @@ public class QueryDslRepositorySupportIntegrationTests {
 		}
 
 		@Bean
-		EntityManagerBeanDefinitionRegistrarPostProcessor entityManagerBeanDefinitionRegistrarPostProcessor() {
+		static EntityManagerBeanDefinitionRegistrarPostProcessor entityManagerBeanDefinitionRegistrarPostProcessor() {
 			return new EntityManagerBeanDefinitionRegistrarPostProcessor();
 		}
 
