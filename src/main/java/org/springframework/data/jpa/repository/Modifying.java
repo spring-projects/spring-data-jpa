7fd992dcddb6ae38cbf6d1b5533fc60ba39533f2
Jens Schauder <jschauder@pivotal.io>
2018-12-19 13:41:06 +0100

    DATAJPA-1485 - Clarified documentation for @Modifying.

--- src/main/java/org/springframework/data/jpa/repository/Modifying.java
+++ src/main/java/org/springframework/data/jpa/repository/Modifying.java
@@ -22,14 +22,21 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * <p>
  * Indicates a query method should be considered as modifying query as that changes the way it needs to be executed.
- * This annotation is only considered if used on actual query methods (either derived or manually defined through a
- * {@link Query} annotation). It's not applied on custom implementation methods as they already have control over the
- * underlying data access APIs.
+ * This annotation is only considered if used on query methods defined through a {@link Query} annotation). It's not
+ * applied on custom implementation methods or queries derived from the method name as they already have control over
+ * the underlying data access APIs or specify if they are modifying by their name.
+ * </p>
+ * <p>
+ * Queries that require a `@Modifying` annotation include {@code INSERT}, {@code UPDATE}, {@code DELETE}, and DDL
+ * statements.
+ * </p>
  * 
  * @author Oliver Gierke
  * @author Christoph Strobl
  * @author Nicolas Cirigliano
+ * @author Jens Schauder
  * @see Query
  */
 @Retention(RetentionPolicy.RUNTIME)
