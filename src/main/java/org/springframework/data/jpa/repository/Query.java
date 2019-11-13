f8047ed9031b5339e89e77ca43638f7c1014e016
tinhcao <caodangtinh@gmail.com>
2019-11-13 23:02:10 +0700

    Update out of date countQuery documentation since it changed to use either originalQuery or countProjection instead of method name from version 1.6

--- src/main/java/org/springframework/data/jpa/repository/Query.java
+++ src/main/java/org/springframework/data/jpa/repository/Query.java
@@ -45,13 +45,13 @@ public @interface Query {
 
 	/**
 	 * Defines a special count query that shall be used for pagination queries to lookup the total number of elements for
-	 * a page. If non is configured we will derive the count query from the method name.
+	 * a page. If non is configured we will derive the count query from the original query or {@link #countProjection()} query if any.
 	 */
 	String countQuery() default "";
 
 	/**
 	 * Defines the projection part of the count query that is generated for pagination. If neither {@link #countQuery()}
-	 * not {@link #countProjection()} is configured we will derive the count query from the method name.
+	 * not {@link #countProjection()} is configured we will derive the count query from the original query.
 	 *
 	 * @return
 	 * @since 1.6
