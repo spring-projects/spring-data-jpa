5481126fa39e17946ac51f50eca90df0b8f382c9
reta <drreta@gmail.com>
2019-12-28 16:48:06 -0500

    DATAJPA-1652 - Support for operators to directly follow named parameters.
    Since `\P{P}` doesn't include things like `<`, `>`, or `|` those where considered part of the parameter name when they weren't separated from it by whitespace.
    This is now fixed.
    
    Original pull request: #405.

--- src/main/java/org/springframework/data/jpa/repository/query/QueryUtils.java
+++ src/main/java/org/springframework/data/jpa/repository/query/QueryUtils.java
@@ -79,6 +79,7 @@ import org.springframework.util.StringUtils;
  * @author Florian Lüdiger
  * @author Grégoire Druant
  * @author Mohammad Hewedy
+ * @author Andriy Redko
  */
 public abstract class QueryUtils {
 
@@ -89,8 +90,8 @@ public abstract class QueryUtils {
 	// Z Separator
 	// Cc Control
 	// Cf Format
-	// P Punctuation
-	private static final String IDENTIFIER = "[._[\\P{Z}&&\\P{Cc}&&\\P{Cf}&&\\P{P}]]+";
+	// Punct Punctuation
+	private static final String IDENTIFIER = "[._$[\\P{Z}&&\\P{Cc}&&\\P{Cf}&&\\P{Punct}]]+";
 	static final String COLON_NO_DOUBLE_COLON = "(?<![:\\\\]):";
 	static final String IDENTIFIER_GROUP = String.format("(%s)", IDENTIFIER);
 
