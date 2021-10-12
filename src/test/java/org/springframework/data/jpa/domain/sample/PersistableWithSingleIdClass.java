8a5588e0005c7b84b78023abf2e9e531bdb96832
Mark Paluch <mpaluch@vmware.com>
2021-10-12 16:08:43 +0200

    Use `@IdClass` as identifier for IdClasses with a single attribute for repository operations.
    We now use the IdClass type for lookups when the entity defines a `@IdClass`. Previously, we uses the type of the defined singular identifier attribute which lead to invalid queries.
    
    Closes #2330

---
+++ src/test/java/org/springframework/data/jpa/domain/sample/PersistableWithSingleIdClass.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.jpa.domain.sample;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+
+/**
+ * Sample entity using {@link IdClass} annotation to demarcate ids.
+ *
+ * @author Mark Paluch
+ */
+@Entity
+@IdClass(PersistableWithSingleIdClassPK.class)
+public class PersistableWithSingleIdClass {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id private Long first;
+
+	protected PersistableWithSingleIdClass() {
+
+	}
+
+	public PersistableWithSingleIdClass(Long first) {
+		this.first = first;
+	}
+
+	/**
+	 * @return the first
+	 */
+	public Long getFirst() {
+		return first;
+	}
+
+}
