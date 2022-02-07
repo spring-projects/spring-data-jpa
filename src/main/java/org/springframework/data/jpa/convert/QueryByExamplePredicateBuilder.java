eac3e7f59d15ec4f0bfb733d4a1428a8a4d52985
Diego Krupitza <info@diegokrupitza.com>
2022-02-07 12:15:27 +0100

    Remove unnecessary `toString()` call.
    There were few explicit `toString()` calls that are not needed, since `toString()` will be called by default.

--- src/main/java/org/springframework/data/jpa/convert/QueryByExamplePredicateBuilder.java
+++ src/main/java/org/springframework/data/jpa/convert/QueryByExamplePredicateBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2021 the original author or authors.
+ * Copyright 2016-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -278,7 +278,7 @@ public class QueryByExamplePredicateBuilder {
 
 			StringBuilder sb = new StringBuilder();
 			if (parent != null) {
-				sb.append(parent.toString());
+				sb.append(parent);
 				sb.append(" -");
 				sb.append(name);
 				sb.append("-> ");
