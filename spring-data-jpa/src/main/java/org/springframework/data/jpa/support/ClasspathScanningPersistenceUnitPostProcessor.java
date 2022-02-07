71c40f1f006bed7ef007ca507cada9c2b34f84c3
Diego Krupitza <info@diegokrupitza.com>
2022-02-07 12:15:27 +0100

    Remove unnecessary `toString()` call.
    There were few explicit `toString()` calls that are not needed, since `toString()` will be called by default.
    
    Original pull request #2431

--- spring-data-jpa/src/main/java/org/springframework/data/jpa/support/ClasspathScanningPersistenceUnitPostProcessor.java
+++ spring-data-jpa/src/main/java/org/springframework/data/jpa/support/ClasspathScanningPersistenceUnitPostProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2021 the original author or authors.
+ * Copyright 2011-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -179,7 +179,7 @@ public class ClasspathScanningPersistenceUnitPostProcessor
 				mappingFileUris.add(resourcePathInClasspath);
 
 			} catch (IOException e) {
-				throw new IllegalStateException(String.format("Couldn't get URI for %s!", resource.toString()), e);
+				throw new IllegalStateException(String.format("Couldn't get URI for %s!", resource), e);
 			}
 		}
 
