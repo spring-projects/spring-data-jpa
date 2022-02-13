e3beadd33f8841e801d6d0de2e7ccbaa1634e89d
jonyschak <jr.onyschak@conduent.com>
2022-02-13 01:31:57 -0600

    Correcting sort object reference in FetchableFluentQueryByExample.sortBy and FetchableFluentQueryByPredicate.sortBy
    Closes #2438
    Original pull request #2439

---
+++ src/test/java/org/springframework/data/jpa/repository/support/FetchableFluentQueryByPredicateUnitTests.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.jpa.repository.support;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
+
+/**
+ * Unit tests for {@link FetchableFluentQueryByPredicate}.
+ *
+ * @author J.R. Onyschak
+ */
+class FetchableFluentQueryByPredicateUnitTests {
+
+	@Test // GH-2438
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	void multipleSortBy() {
+		Sort s1 = Sort.by(Order.by("s1"));
+		Sort s2 = Sort.by(Order.by("s2"));
+		FetchableFluentQueryByPredicate f = new FetchableFluentQueryByPredicate(null, null, null, null, null, null, null);
+		f = (FetchableFluentQueryByPredicate) f.sortBy(s1).sortBy(s2);
+		assertThat(f.sort).isEqualTo(s1.and(s2));
+	}
+}
