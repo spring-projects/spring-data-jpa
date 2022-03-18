b0edbf0723df57168ef5da6fbec1cf89b0df2d94
Greg L. Turnquist <gturnquist@vmware.com>
2022-03-18 16:24:15 -0500

    Add tests to verify AuditingEntityListener works with embeddables.
    See #2365.

---
+++ src/test/java/org/springframework/data/jpa/domain/sample/AuditableEmbeddable.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2008-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.jpa.domain.sample;
+
+import java.time.Instant;
+
+import javax.persistence.Embeddable;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+/**
+ * JPA {@link Embeddable} to test out {@link org.springframework.data.jpa.domain.support.AuditingEntityListener}.
+ * 
+ * @author Greg Turnquist
+ */
+@Embeddable
+public class AuditableEmbeddable {
+
+	@CreatedDate //
+	private Instant dateCreated;
+
+	@LastModifiedDate //
+	private Instant dateUpdated;
+
+	public Instant getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Instant dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public Instant getDateUpdated() {
+		return dateUpdated;
+	}
+
+	public void setDateUpdated(Instant dateUpdated) {
+		this.dateUpdated = dateUpdated;
+	}
+}
---
+++ src/test/java/org/springframework/data/jpa/domain/sample/AuditableEntity.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2008-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.jpa.domain.sample;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+/**
+ * JPA entity with an {@link Embedded} set of auditable data.
+ *
+ * @author Greg Turnquist
+ */
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class AuditableEntity {
+
+	@Id
+	@GeneratedValue //
+	private Long id;
+
+	private String data;
+
+	@Embedded //
+	private AuditableEmbeddable auditDetails;
+
+	public AuditableEntity() {
+		this(null, null, null);
+	}
+
+	public AuditableEntity(Long id, String data, AuditableEmbeddable auditDetails) {
+
+		this.id = id;
+		this.data = data;
+		this.auditDetails = auditDetails;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public AuditableEmbeddable getAuditDetails() {
+		return auditDetails;
+	}
+
+	public void setAuditDetails(AuditableEmbeddable auditDetails) {
+		this.auditDetails = auditDetails;
+	}
+}
