75c9af5320e34bbc0b60808403ed04768674dc7b
Greg L. Turnquist <gturnquist@vmware.com>
2022-04-13 11:01:19 -0500

    Convert Iterable<ID> to Collection<ID> for deleteAllByIdInBatch.
    JpaRepository accepts Iterable<ID> for bulk deletes. But some JPA providers require Collection<ID> instead. To avoid breaking any APIs, convert the incoming argument if it's not already a Collection.
    
    See #2242.

--- src/main/java/org/springframework/data/jpa/repository/support/SimpleJpaRepository.java
+++ src/main/java/org/springframework/data/jpa/repository/support/SimpleJpaRepository.java
@@ -24,6 +24,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import javax.persistence.EntityManager;
 import javax.persistence.LockModeType;
@@ -234,7 +236,16 @@ public class SimpleJpaRepository<T, ID> implements JpaRepositoryImplementation<T
 					entityInformation.getIdAttribute().getName());
 
 			Query query = em.createQuery(queryString);
-			query.setParameter("ids", ids);
+			/**
+			 * Some JPA providers require {@code ids} to be a {@link Collection} so we must convert if it's not already.
+			 */
+			if (Collection.class.isInstance(ids)) {
+				query.setParameter("ids", ids);
+			} else {
+				Collection<ID> idsCollection = StreamSupport.stream(ids.spliterator(), false)
+						.collect(Collectors.toCollection(ArrayList::new));
+				query.setParameter("ids", idsCollection);
+			}
 
 			query.executeUpdate();
 		}
