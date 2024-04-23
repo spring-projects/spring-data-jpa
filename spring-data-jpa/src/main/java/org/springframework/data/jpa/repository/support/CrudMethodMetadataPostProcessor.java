57dc9d774ca34b25f5b0f3c9585e5ad15bda6058
JANG <gorae930301@gmail.com>
2024-04-24 00:50:30 +0900

    Use Assert.notNull

--- spring-data-jpa/src/main/java/org/springframework/data/jpa/repository/support/CrudMethodMetadataPostProcessor.java
+++ spring-data-jpa/src/main/java/org/springframework/data/jpa/repository/support/CrudMethodMetadataPostProcessor.java
@@ -119,10 +119,9 @@ class CrudMethodMetadataPostProcessor implements RepositoryProxyPostProcessor, B
 
 			MethodInvocation mi = currentInvocation.get();
 
-			if (mi == null)
-				throw new IllegalStateException(
-						"No MethodInvocation found: Check that an AOP invocation is in progress, and that the "
+			Assert.notNull(mi, "No MethodInvocation found: Check that an AOP invocation is in progress, and that the "
 								+ "CrudMethodMetadataPopulatingMethodInterceptor is upfront in the interceptor chain.");
+
 			return mi;
 		}
 
