55bc593b0832a2357016a78d28853e84ff692942
Christoph Strobl <christoph.strobl@broadcom.com>
2024-09-26 11:31:05 +0200

    Add configuration option to disable usage of JSqlParser for native queries.
    This commit introduces the spring.data.jpa.query.native.parser property that allows to switch native query parsing to the default internal parser for scenarios where JSqlParser is on the classpath but should not be used by spring-data.

--- spring-data-jpa/src/main/java/org/springframework/data/jpa/repository/query/QueryEnhancerFactory.java
+++ spring-data-jpa/src/main/java/org/springframework/data/jpa/repository/query/QueryEnhancerFactory.java
@@ -17,8 +17,11 @@ package org.springframework.data.jpa.repository.query;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.core.SpringProperties;
 import org.springframework.data.jpa.provider.PersistenceProvider;
+import org.springframework.lang.Nullable;
 import org.springframework.util.ClassUtils;
+import org.springframework.util.StringUtils;
 
 /**
  * Encapsulates different strategies for the creation of a {@link QueryEnhancer} from a {@link DeclaredQuery}.
@@ -26,20 +29,17 @@ import org.springframework.util.ClassUtils;
  * @author Diego Krupitza
  * @author Greg Turnquist
  * @author Mark Paluch
+ * @author Christoph Strobl
  * @since 2.7.0
  */
 public final class QueryEnhancerFactory {
 
 	private static final Log LOG = LogFactory.getLog(QueryEnhancerFactory.class);
-
-	private static final boolean jSqlParserPresent = ClassUtils.isPresent("net.sf.jsqlparser.parser.JSqlParser",
-			QueryEnhancerFactory.class.getClassLoader());
+	private static final NativeQueryEnhancer NATIVE_QUERY_ENHANCER;
 
 	static {
 
-		if (jSqlParserPresent) {
-			LOG.info("JSqlParser is in classpath; If applicable, JSqlParser will be used");
-		}
+		NATIVE_QUERY_ENHANCER = NativeQueryEnhancer.select(QueryEnhancerFactory.class.getClassLoader());
 
 		if (PersistenceProvider.ECLIPSELINK.isPresent()) {
 			LOG.info("EclipseLink is in classpath; If applicable, EQL parser will be used.");
@@ -48,7 +48,6 @@ public final class QueryEnhancerFactory {
 		if (PersistenceProvider.HIBERNATE.isPresent()) {
 			LOG.info("Hibernate is in classpath; If applicable, HQL parser will be used.");
 		}
-
 	}
 
 	private QueryEnhancerFactory() {}
@@ -62,15 +61,7 @@ public final class QueryEnhancerFactory {
 	public static QueryEnhancer forQuery(DeclaredQuery query) {
 
 		if (query.isNativeQuery()) {
-
-			if (jSqlParserPresent) {
-				/*
-				 * If JSqlParser fails, throw some alert signaling that people should write a custom Impl.
-				 */
-				return new JSqlParserQueryEnhancer(query);
-			}
-
-			return new DefaultQueryEnhancer(query);
+			return getNativeQueryEnhancer(query);
 		}
 
 		if (PersistenceProvider.HIBERNATE.isPresent()) {
@@ -79,6 +70,58 @@ public final class QueryEnhancerFactory {
 			return JpaQueryEnhancer.forEql(query);
 		} else {
 			return JpaQueryEnhancer.forJpql(query);
+		}
+	}
+
+	/**
+	 * Get the native query enhancer for the given {@link DeclaredQuery query} based on {@link #NATIVE_QUERY_ENHANCER}.
+	 *
+	 * @param query the declared query.
+	 * @return new instance of {@link QueryEnhancer}.
+	 */
+	private static QueryEnhancer getNativeQueryEnhancer(DeclaredQuery query) {
+
+		if (NATIVE_QUERY_ENHANCER.equals(NativeQueryEnhancer.JSQL)) {
+			return new JSqlParserQueryEnhancer(query);
+		}
+		return new DefaultQueryEnhancer(query);
+	}
+
+	/**
+	 * Possible choices for the {@link #NATIVE_PARSER_PROPERTY}. Read current selection via {@link #select(ClassLoader)}.
+	 */
+	enum NativeQueryEnhancer {
+
+		AUTO, DEFAULT, JSQL;
+
+		static final String NATIVE_PARSER_PROPERTY = "spring.data.jpa.query.native.parser";
+
+		private static NativeQueryEnhancer from(@Nullable String name) {
+			if (!StringUtils.hasText(name)) {
+				return AUTO;
+			}
+			return NativeQueryEnhancer.valueOf(name.toUpperCase());
+		}
+
+		/**
+		 * @param classLoader ClassLoader to look up available libraries.
+		 * @return the current selection considering classpath avialability and user selection via
+		 *         {@link #NATIVE_PARSER_PROPERTY}.
+		 */
+		static NativeQueryEnhancer select(ClassLoader classLoader) {
+
+			if (!ClassUtils.isPresent("net.sf.jsqlparser.parser.JSqlParser", classLoader)) {
+				return NativeQueryEnhancer.DEFAULT;
+			}
+
+			NativeQueryEnhancer selected = NativeQueryEnhancer.from(SpringProperties.getProperty(NATIVE_PARSER_PROPERTY));
+			if (selected.equals(NativeQueryEnhancer.AUTO) || selected.equals(NativeQueryEnhancer.JSQL)) {
+				LOG.info("JSqlParser is in classpath; If applicable, JSqlParser will be used.");
+				return NativeQueryEnhancer.JSQL;
+			}
+
+			LOG.info("JSqlParser is in classpath but won't be used due to user choice.");
+			return selected;
 		}
 	}
 
