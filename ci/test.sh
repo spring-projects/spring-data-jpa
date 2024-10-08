27579609227b8a09de98f13770e01fc17219288e
Mark Paluch <mark.paluch@broadcom.com>
2024-10-08 11:19:00 +0200

    Consistently run all CI steps with the same user.
    See #3610

--- ci/test.sh
+++ ci/test.sh
@@ -4,15 +4,12 @@ set -euo pipefail
 
 mkdir -p /tmp/jenkins-home/.m2/spring-data-jpa
 mkdir -p /tmp/jenkins-home/.m2/.develocity
-chown -R 1001:1001 .
 
 export JENKINS_USER=${JENKINS_USER_NAME}
 
 MAVEN_OPTS="-Duser.name=${JENKINS_USER} -Duser.home=/tmp/jenkins-home" \
   ./mvnw -s settings.xml  \
-  -P${PROFILE} clean dependency:list test -Dsort -U -B -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-jpa
+  -P${PROFILE} clean dependency:list test -Dsort -U -B -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-jpa -Ddevelocity.storage.directory=/tmp/jenkins-home/.develocity-root
 
 MAVEN_OPTS="-Duser.name=${JENKINS_USER} -Duser.home=/tmp/jenkins-home" \
-  ./mvnw -s settings.xml clean -Dscan=false -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-jpa
-
-chown -R 1001:1001  /tmp/jenkins-home/.m2/.develocity
+  ./mvnw -s settings.xml clean -Dscan=false -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-jpa -Ddevelocity.storage.directory=/tmp/jenkins-home/.develocity-root
