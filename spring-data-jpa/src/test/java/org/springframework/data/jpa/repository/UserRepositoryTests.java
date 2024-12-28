c5eedfd7fd624f5357c14eb8d60090c40bc7702e
Joshua Chen <27291761@qq.com>
2024-12-28 15:02:46 +0800

    Support custom countSpec in SimpleJpaRepository.findAll(…).
    Closes #3727

--- spring-data-jpa/src/test/java/org/springframework/data/jpa/repository/UserRepositoryTests.java
+++ spring-data-jpa/src/test/java/org/springframework/data/jpa/repository/UserRepositoryTests.java
@@ -47,7 +47,6 @@ import java.util.stream.Stream;
 import org.assertj.core.api.SoftAssertions;
 import org.hibernate.LazyInitializationException;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
