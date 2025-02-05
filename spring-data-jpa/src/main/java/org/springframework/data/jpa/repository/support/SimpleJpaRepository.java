b90ab188b3dc4a955c53d0eb612e6ba3cb57902f
Mark Paluch <mark.paluch@broadcom.com>
2025-02-05 08:54:33 +0100

    Consider sort order for unpaged Pageable in Querydsl.
    We now apply sorting using Querydsl for Pageable that is sorted but not paged.
    
    Closes #3761

--- spring-data-jpa/src/main/java/org/springframework/data/jpa/repository/support/SimpleJpaRepository.java
+++ spring-data-jpa/src/main/java/org/springframework/data/jpa/repository/support/SimpleJpaRepository.java
@@ -738,7 +738,6 @@ public class SimpleJpaRepository<T, ID> implements JpaRepositoryImplementation<T
 	 * @param pageable must not be {@literal null}.
 	 */
 	protected TypedQuery<T> getQuery(@Nullable Specification<T> spec, Pageable pageable) {
-
 		return getQuery(spec, getDomainClass(), pageable.getSort());
 	}
 
@@ -751,7 +750,6 @@ public class SimpleJpaRepository<T, ID> implements JpaRepositoryImplementation<T
 	 */
 	protected <S extends T> TypedQuery<S> getQuery(@Nullable Specification<S> spec, Class<S> domainClass,
 			Pageable pageable) {
-
 		return getQuery(spec, domainClass, pageable.getSort());
 	}
 
