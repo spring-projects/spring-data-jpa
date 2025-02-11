677f1d78f63e2f982ffb4d1354c3cbd2354b2837
Mark Paluch <mark.paluch@broadcom.com>
2025-02-11 14:28:29 +0100

    Bump memory to 4G.
    See #3767

--- ci/test.sh
+++ ci/test.sh
@@ -7,7 +7,7 @@ mkdir -p /tmp/jenkins-home/.m2/.develocity
 
 export JENKINS_USER=${JENKINS_USER_NAME}
 
-MAVEN_OPTS="-Duser.name=${JENKINS_USER} -Duser.home=/tmp/jenkins-home" \
+MAVEN_OPTS="-Xmx4G -Duser.name=${JENKINS_USER} -Duser.home=/tmp/jenkins-home" \
   ./mvnw -s settings.xml  \
   -P${PROFILE} clean dependency:list test -Dsort -U -B -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-jpa -Ddevelocity.storage.directory=/tmp/jenkins-home/.develocity-root
 
