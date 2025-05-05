b3c0210f17ae87e7e13efcc857e4d07335fb432d
Diego Pedregal <diego.p.hidalgo@gmail.com>
2025-05-05 11:13:59 +0200

    Removes `PlainSelect` casting in `JSqlParserQueryEnhancer`.
    Closes: #3869
    Original pull request: #3870
    
    Signed-off-by: Diego Pedregal <diego.p.hidalgo@gmail.com>

--- spring-data-jpa/src/test/java/org/springframework/data/jpa/repository/query/JSqlParserQueryEnhancerUnitTests.java
+++ spring-data-jpa/src/test/java/org/springframework/data/jpa/repository/query/JSqlParserQueryEnhancerUnitTests.java
@@ -289,4 +289,13 @@ class JSqlParserQueryEnhancerUnitTests extends QueryEnhancerTckTests {
 				ReturnedType.of(Object.class, Object.class, new SpelAwareProxyProjectionFactory()));
 	}
 
+	@Test // GH-3869
+	void shouldWorkWithoutFromClause() {
+		String query = "SELECT is_contained_in(:innerId, :outerId)";
+
+		StringQuery stringQuery = new StringQuery(query, true);
+
+		assertThat(stringQuery.getQueryString()).isEqualTo(query);
+	}
+
 }
