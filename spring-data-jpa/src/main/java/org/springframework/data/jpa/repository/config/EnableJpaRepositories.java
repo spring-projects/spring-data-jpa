b8ebcf9ded607e6deffca022847ecd21d9c0a2e7
Mark Paluch <mark.paluch@broadcom.com>
2025-09-19 09:23:25 +0200

    Document placeholder and Ant-style pattern support for `@Enable…Repositories`
    Closes spring-projects/spring-data-commons#3366

--- spring-data-jpa/src/main/java/org/springframework/data/jpa/repository/config/EnableJpaRepositories.java
+++ spring-data-jpa/src/main/java/org/springframework/data/jpa/repository/config/EnableJpaRepositories.java
@@ -57,8 +57,20 @@ public @interface EnableJpaRepositories {
 	String[] value() default {};
 
 	/**
-	 * Base packages to scan for annotated components. {@link #value()} is an alias for (and mutually exclusive with) this
-	 * attribute. Use {@link #basePackageClasses()} for a type-safe alternative to String-based package names.
+	 * Base packages to scan for annotated components.
+	 * <p>
+	 * {@link #value} is an alias for (and mutually exclusive with) this attribute.
+	 * <p>
+	 * Supports {@code ${…}} placeholders which are resolved against the {@link org.springframework.core.env.Environment
+	 * Environment} as well as Ant-style package patterns &mdash; for example, {@code "org.example.**"}.
+	 * <p>
+	 * Multiple packages or patterns may be specified, either separately or within a single {@code String} &mdash; for
+	 * example, {@code {"org.example.config", "org.example.service.**"}} or
+	 * {@code "org.example.config, org.example.service.**"}.
+	 * <p>
+	 * Use {@link #basePackageClasses} for a type-safe alternative to String-based package names.
+	 *
+	 * @see org.springframework.context.ConfigurableApplicationContext#CONFIG_LOCATION_DELIMITERS
 	 */
 	String[] basePackages() default {};
 
