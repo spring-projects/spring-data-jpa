600d34165b265b1a11c1aa8c4224ab3e66d8f0a6
Christoph Strobl <christoph.strobl@broadcom.com>
2025-11-27 13:26:55 +0100

    Polishing.
    Limit ability to override properties.
    
    Original Pull Request: #4106

--- spring-data-jpa/src/test/java/org/springframework/data/jpa/repository/aot/AotMetamodelUnitTests.java
+++ spring-data-jpa/src/test/java/org/springframework/data/jpa/repository/aot/AotMetamodelUnitTests.java
@@ -15,14 +15,19 @@
  */
 package org.springframework.data.jpa.repository.aot;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.data.jpa.repository.aot.AotMetamodel.NoOpConnectionProvider;
+import org.springframework.data.jpa.repository.aot.AotMetamodel.SpringDataJpaAotDialect;
 
 /**
  * Unit tests for {@link AotMetamodel}.
  *
  * @author Mark Paluch
+ * @author Christoph Strobl
  */
 class AotMetamodelUnitTests {
 
@@ -30,5 +35,37 @@ class AotMetamodelUnitTests {
 	void dialectSupportsSequences() {
 		assertThat(AotMetamodel.SpringDataJpaAotDialect.INSTANCE.getSequenceSupport().supportsSequences()).isTrue();
 		assertThat(AotMetamodel.SpringDataJpaAotDialect.INSTANCE.getSequenceSupport().supportsPooledSequences()).isTrue();
+	}
+
+	@Test // GH-4092
+	void intializesPropertiesWithDefaults() {
+
+		assertThat(AotMetamodel.initProperties(Map.of())) //
+				.containsEntry("hibernate.dialect", SpringDataJpaAotDialect.INSTANCE) //
+				.containsEntry("hibernate.boot.allow_jdbc_metadata_access", false) //
+				.containsEntry("hibernate.connection.provider_class", NoOpConnectionProvider.INSTANCE) //
+				.containsEntry("hibernate.jpa_callbacks.enabled", false) //
+				.containsEntry("hibernate.query.startup_check", false);
+	}
+
+	@Test // GH-4092
+	void allowsDialectOverridesViaProperties() {
+
+		assertThat(AotMetamodel.initProperties(Map.of("hibernate.dialect", "H2Dialect", "jpa.bla.bla", "42")))
+				.containsEntry("hibernate.dialect", "H2Dialect") //
+				.containsEntry("jpa.bla.bla", "42");
+	}
+
+	@Test // GH-4092
+	void preventsPropertyOverrides/* for cases we know cause trouble */() {
+
+		assertThat(AotMetamodel.initProperties(Map.of(//
+				"hibernate.boot.allow_jdbc_metadata_access", "true", //
+				"hibernate.connection.provider_class", "DatasourceConnectionProviderImpl", //
+				"hibernate.jpa_callbacks.enabled", "true", //
+				"hibernate.query.startup_check", "true"))).containsEntry("hibernate.boot.allow_jdbc_metadata_access", false) //
+				.containsEntry("hibernate.connection.provider_class", NoOpConnectionProvider.INSTANCE) //
+				.containsEntry("hibernate.jpa_callbacks.enabled", false) //
+				.containsEntry("hibernate.query.startup_check", false);
 	}
 }
