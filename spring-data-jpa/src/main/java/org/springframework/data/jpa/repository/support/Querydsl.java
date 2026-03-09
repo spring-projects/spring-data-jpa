cb2326bed72b747544a900f55621feba5e9a2ac8
Kamil Krzywanski <kamilkrzywanski01@gmail.com>
2026-03-09 14:42:04 +0100

    Issue #4209 - fix for querydsl queries by aliases - add unsafe JpaSort
    Signed-off-by: Kamil Krzywanski <kamilkrzywanski01@gmail.com>

--- spring-data-jpa/src/main/java/org/springframework/data/jpa/repository/support/Querydsl.java
+++ spring-data-jpa/src/main/java/org/springframework/data/jpa/repository/support/Querydsl.java
@@ -15,6 +15,7 @@
  */
 package org.springframework.data.jpa.repository.support;
 
+import com.querydsl.core.types.dsl.StringExpression;
 import jakarta.persistence.EntityManager;
 
 import java.util.List;
@@ -24,6 +25,7 @@ import org.springframework.data.core.PropertyReferenceException;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Order;
+import org.springframework.data.jpa.domain.JpaSort;
 import org.springframework.data.jpa.provider.PersistenceProvider;
 import org.springframework.data.jpa.repository.query.QueryUtils;
 import org.springframework.data.querydsl.QSort;
@@ -229,6 +231,12 @@ public class Querydsl {
     private Expression<?> buildOrderPropertyPathFrom(Order order, JPQLQuery<?> query) {
 
         Assert.notNull(order, "Order must not be null");
+
+        if (order instanceof JpaSort.JpaOrder jpaOrder && jpaOrder.isUnsafe()) {
+            QueryUtils.checkSortExpression(order);
+            return Expressions.template(Object.class, order.getProperty());
+        }
+
         Expression<?> sortPropertyExpression;
         try {
 
