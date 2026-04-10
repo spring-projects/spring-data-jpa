a3fa1c9eedda522e82837cc7ab8b7c64e71ccc88
Mark Paluch <mark.paluch@broadcom.com>
2026-04-10 16:34:44 +0200

    Polishing.
    Consistently handle unsafe sort as fallback and not as a initial entry option.
    
    See #4209
    Original pull request: #4211

--- spring-data-jpa/src/main/java/org/springframework/data/jpa/repository/support/Querydsl.java
+++ spring-data-jpa/src/main/java/org/springframework/data/jpa/repository/support/Querydsl.java
@@ -26,6 +26,7 @@ import org.springframework.data.jpa.domain.JpaSort;
 import org.springframework.data.jpa.provider.PersistenceProvider;
 import org.springframework.data.jpa.repository.query.QueryUtils;
 import org.springframework.data.mapping.PropertyPath;
+import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.data.querydsl.QSort;
 import org.springframework.lang.Nullable;
 import org.springframework.util.Assert;
@@ -185,7 +186,7 @@ public class Querydsl {
 		Assert.notNull(query, "Query must not be null");
 
 		for (Order order : sort) {
-			query.orderBy(toOrderSpecifier(order, query));
+			query.orderBy(toOrderSpecifier(order));
 		}
 
 		return query;
@@ -197,11 +198,11 @@ public class Querydsl {
 	 * @param order must not be {@literal null}.
 	 */
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	private OrderSpecifier<?> toOrderSpecifier(Order order, JPQLQuery<?> query) {
+	private OrderSpecifier<?> toOrderSpecifier(Order order) {
 
 		return new OrderSpecifier(
 				order.isAscending() ? com.querydsl.core.types.Order.ASC : com.querydsl.core.types.Order.DESC,
-				buildOrderPropertyPathFrom(order, query), toQueryDslNullHandling(order.getNullHandling()));
+				buildOrderPropertyPathFrom(order), toQueryDslNullHandling(order.getNullHandling()));
 	}
 
 	/**
@@ -227,29 +228,34 @@ public class Querydsl {
 	 *
 	 * @param order must not be {@literal null}.
 	 */
-	private Expression<?> buildOrderPropertyPathFrom(Order order, JPQLQuery<?> query) {
+	private Expression<?> buildOrderPropertyPathFrom(Order order) {
 
-			Assert.notNull(order, "Order must not be null");
+		Assert.notNull(order, "Order must not be null");
 
-				if (order instanceof JpaSort.JpaOrder jpaOrder && jpaOrder.isUnsafe()) {
-					return Expressions.template(Object.class, order.getProperty());
-				}
+		QueryUtils.checkSortExpression(order);
 
-				QueryUtils.checkSortExpression(order);
-				PropertyPath path = PropertyPath.from(order.getProperty(), builder.getType());
-				Expression<?> sortPropertyExpression = builder;
+		try {
+			PropertyPath path = PropertyPath.from(order.getProperty(), builder.getType());
+			Expression<?> sortPropertyExpression = builder;
 
-				while (path != null) {
+			while (path != null) {
 
-					sortPropertyExpression = !path.hasNext() && order.isIgnoreCase() && String.class.equals(path.getType()) //
-							? Expressions.stringPath((Path<?>) sortPropertyExpression, path.getSegment()).lower() //
-							: Expressions.path(path.getType(), (Path<?>) sortPropertyExpression, path.getSegment());
+				sortPropertyExpression = !path.hasNext() && order.isIgnoreCase() && String.class.equals(path.getType()) //
+						? Expressions.stringPath((Path<?>) sortPropertyExpression, path.getSegment()).lower() //
+						: Expressions.path(path.getType(), (Path<?>) sortPropertyExpression, path.getSegment());
 
-						path = path.next();
-					}
-
-				return sortPropertyExpression;
+				path = path.next();
 			}
+
+			return sortPropertyExpression;
+		} catch (PropertyReferenceException e) {
+			if (order instanceof JpaSort.JpaOrder jpaOrder && jpaOrder.isUnsafe()) {
+				return Expressions.template(Object.class, order.getProperty());
+			} else {
+				throw e;
+			}
+		}
+	}
 
 	/**
 	 * Creates an {@link Expression} for the given {@code property} property.
