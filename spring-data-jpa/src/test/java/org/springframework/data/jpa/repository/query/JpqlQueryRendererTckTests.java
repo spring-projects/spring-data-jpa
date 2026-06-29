ab720662ee1a2bfac4453fcdf10fc8419fca38b0
jewoodev <jewoos15@naver.com>
2026-06-29 16:56:59 +0900

    Allow map key and value expressions in collection membership.
    Allow `KEY(...)` and `VALUE(...)` expressions as collection membership predicate left operands in JPQL and EQL.
    
    Add shared renderer TCK coverage for `MEMBER OF` and `NOT MEMBER OF` with map key and value expressions.
    
    Signed-off-by: jewoodev <jewoos15@naver.com>

--- spring-data-jpa/src/test/java/org/springframework/data/jpa/repository/query/JpqlQueryRendererTckTests.java
+++ spring-data-jpa/src/test/java/org/springframework/data/jpa/repository/query/JpqlQueryRendererTckTests.java
@@ -744,6 +744,28 @@ abstract class JpqlQueryRendererTckTests {
 	}
 
 	@Test
+	void mapFieldIdentificationVariableAsCollectionMemberExpression() {
+
+		assertQuery("""
+				SELECT i
+				FROM Item i JOIN i.photos p
+				WHERE VALUE(p) MEMBER OF i.photoValues
+				""");
+
+		assertQuery("""
+				SELECT i
+				FROM Item i JOIN i.photos p
+				WHERE KEY(p) MEMBER OF i.photoKeys
+				""");
+
+		assertQuery("""
+				SELECT i
+				FROM Item i JOIN i.photos p
+				WHERE VALUE(p) NOT MEMBER OF i.photoValues
+				""");
+	}
+
+	@Test
 	void existsSubSelectExample1() {
 
 		assertQuery("""
